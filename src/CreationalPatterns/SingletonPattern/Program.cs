29c0b486f19a011f90bfd35bc9ba1b463d3645ea
Tarek Mostafa <tarek.r.mostafa@gmail.com>
2025-02-08 21:51:28 +0200

    Add SingletonPattern project with Singleton implementation

---
+++ src/CreationalPatterns/SingletonPattern/Program.cs
@@ -0,0 +1,49 @@
+﻿// singleton pattern is a creational pattern that ensures a class has
+// only one instance and provides a global point of access to it.
+
+
+namespace SingletonPattern
+{
+	public sealed class Singleton
+	{
+		private static readonly Lock Padlock = new Lock();
+		private static Singleton _instance = null;
+
+		public static Singleton Instance
+		{
+			get
+			{
+				lock (Padlock)
+				{
+					return _instance ??= new Singleton();
+				}
+			}
+		}
+
+		private Singleton()
+		{
+			// Private constructor to prevent instantiation
+		}
+
+		public void DoSomething()
+		{
+			Console.WriteLine("Singleton instance is working.");
+		}
+	}
+
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Singleton singleton1 = Singleton.Instance;
+			Singleton singleton2 = Singleton.Instance;
+
+			if (singleton1 == singleton2)
+			{
+				Console.WriteLine("Both instances are the same.");
+			}
+
+			singleton1.DoSomething();
+		}
+	}
+}
